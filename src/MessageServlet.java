54a3e44ad056b92c3c4a56becdf3bf6e5ec3972c
ashton <goashton@dharma.com>
2018-05-04 00:22:17 +0800

    Add jsp demo

---
+++ src/MessageServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MessageServlet", urlPatterns = "/message")
+public class MessageServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String message = request.getParameter("message");
+        request.setAttribute("text", message);
+        request.getRequestDispatcher("/binding.jsp").forward(request, response);
+    }
+}
